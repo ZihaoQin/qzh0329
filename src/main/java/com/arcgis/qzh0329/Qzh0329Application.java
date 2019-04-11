3dc62e5550a66ef14c971fc314c20b563ad673b1
ZihaoQin <302763362@qq.com>
2019-04-11 20:01:58 +0800

    Initial commit

---
+++ src/main/java/com/arcgis/qzh0329/Qzh0329Application.java
@@ -0,0 +1,13 @@
+package com.arcgis.qzh0329;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Qzh0329Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Qzh0329Application.class, args);
+	}
+
+}
